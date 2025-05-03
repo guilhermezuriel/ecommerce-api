c8e4fbbcaa126f9559bed2ab2d3d3dff62af7762
Guilherme Zuriel <guizuriel.dev@gmail.com>
2025-05-03 15:23:08 -0300

    feature - Created public object validator to every class in the project

---
+++ src/main/java/com/guilhermezuriel/ecommerce_api/shared/PublicValidator.java
@@ -0,0 +1,28 @@
+package com.guilhermezuriel.ecommerce_api.shared;
+
+import jakarta.validation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class PublicValidator {
+
+    private static final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    private static final Validator validator = factory.getValidator();
+
+
+    public static String validate(Object item, boolean throwException) {
+        Set<ConstraintViolation<Object>> violations = validator.validate(item);
+        Map<String, String> errors = new HashMap<>();
+        if (!violations.isEmpty()) {
+            for (ConstraintViolation<Object> violation : violations) {
+                errors.put(violation.getPropertyPath().toString(), violation.getMessage());
+            }
+            if (throwException) {
+                throw new ConstraintViolationException("Validation failed", violations);
+            }
+        }
+        return errors.values().toString();
+    }
+}
