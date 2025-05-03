3a0ee0216c6a7ef874e8be1574304e6c0baf4dfa
Guilherme Zuriel <guizuriel.dev@gmail.com>
2025-05-03 15:24:57 -0300

    feature - Created method to validate user by email

--- src/main/java/com/guilhermezuriel/ecommerce_api/user/infra/repository/UserRepository.java
+++ src/main/java/com/guilhermezuriel/ecommerce_api/user/infra/repository/UserRepository.java
@@ -1,10 +1,9 @@
 package com.guilhermezuriel.ecommerce_api.user.infra.repository;
 
-import com.guilhermezuriel.ecommerce_api.user.infra.persistence.UserEntity;
+import com.guilhermezuriel.ecommerce_api.user.infra.entity.UserEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<UserEntity, String> {
 
-
-
+    boolean existsByEmail(String email);
 }
