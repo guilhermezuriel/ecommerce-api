1a7f581b304fd2780037a726ca9115d4246fcf8f
Guilherme Zuriel <guizuriel.dev@gmail.com>
2025-05-03 15:24:07 -0300

    feature - User subscription form input

---
+++ src/main/java/com/guilhermezuriel/ecommerce_api/user/form/CreateUserForm.java
@@ -0,0 +1,15 @@
+package com.guilhermezuriel.ecommerce_api.user.form;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateUserForm {
+    private  String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+}
