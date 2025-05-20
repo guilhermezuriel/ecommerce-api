287766dcd72382f8c0c49445c393dda6737b3466
Guilherme Zuriel <guizuriel.dev@gmail.com>
2025-05-20 16:09:03 -0300

    adding @Repository annotation

--- src/main/java/com/guilhermezuriel/ecommerce_api/user/infra/repository/UserRepository.java
+++ src/main/java/com/guilhermezuriel/ecommerce_api/user/infra/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.guilhermezuriel.ecommerce_api.user.infra.repository;
 
 import com.guilhermezuriel.ecommerce_api.user.infra.entity.UserEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends MongoRepository<UserEntity, String> {
 
     boolean existsByEmail(String email);
